64962ef33927e9aeb18d7a1ccdbfe5a6173b832f
hafsamhand <hafsamhand@gmail.com>
2026-03-31 18:03:49 +0100

    test render

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Test from '../pages/Dashboard/test';
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/" element={<Test />} />
         <Route path="/login" element={<h1>Login</h1>} />
         <Route path="/register" element={<h1>Register</h1>} />
         <Route path="/notes" element={<h1>Notes</h1>} />
